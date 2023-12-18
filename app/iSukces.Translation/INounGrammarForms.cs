a2b18624c3f557441f68e0271dc69745d1d4f622
Piotr Stęclik <piotr.steclik@isukces.com>
2023-12-18 09:22:08 +0100

    adds INounGrammarForms interface

---
+++ app/iSukces.Translation/INounGrammarForms.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace iSukces.Translation;
+
+public interface INounGrammarForms
+{
+    string Choose(int number, params string[] forms);
+
+    CultureInfo Culture { get; }
+}
