3319c68634cb4ada0035b6dc21ed58d39742cf4e
Piotr Stęclik <piotr@steclik.com>
2024-06-30 15:51:31 +0200

    adds method to choosing proper form of polish nouns

---
+++ app/iSukces.Translation/PolishGrammar.cs
@@ -0,0 +1,15 @@
+﻿namespace iSukces.Translation;
+
+public static class PolishGrammar
+{
+    public static string NounForm(int number, string form1, string form2, string form3)
+    {
+        if (number == 1)
+            return form1;
+        number %= 100;
+        if (number is > 10 and < 20)
+            return form3;
+        number %= 10;
+        return number < 5 ? form2 : form3;
+    }
+}
